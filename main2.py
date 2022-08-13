d24133b49f35b90869abf0a46add721719d25937
Joe Vasquez <joseph.c.vasquez@gmail.com>
2022-08-12 20:58:09 -0700

    Creating RDDs and ability to find entities in a string

--- main2.py
+++ main2.py
@@ -1,24 +1,55 @@
 
 import nltk
+import pyspark
 
-nltk.download('punkt')
+#Use first time to download key packages
+"""nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('maxent_ne_chunker')
-nltk.download('words')
+nltk.download('words')"""
 
-from nltk.tokenize import word_tokenize
-from nltk.tag import pos_tag
-from nltk.chunk import ne_chunk
-sentence = 'European authorities fined Google a record $5.1 billion on Wednesday for abusing its power in the mobile phone market and ordered the company to alter its practices'
-ne_tree = ne_chunk(pos_tag(word_tokenize(sentence)))
-print(ne_tree)
+#JAVA_HOME = /usr/lib/jvm/java-8-openjdk-amd64
+from nltk import ne_chunk, pos_tag, word_tokenize
+from nltk.tree import Tree
 
-ex = 'European authorities fined Google a record $5.1 billion on Wednesday for abusing its power in the mobile phone market and ordered the company to alter its practices'
+sc = pyspark.SparkContext()
 
-def preprocess(sent):
-    sent = nltk.word_tokenize(sent)
-    sent = nltk.pos_tag(sent)
-    return sent
+#Array of file names
+files = [
+    "data/data/abc_news_86680728811.csv",
+    "data/data/fox_news_15704546335.csv",
+    "data/data/bbc_228735667216.csv",
+    "data/data/nbc_news_155869377766434.csv",
+    "data/data/cbs_news_131459315949.csv",
+    "data/data/npr_10643211755.csv",
+    "data/data/cnn_5550296508.csv",
+    "data/data/the_los_angeles_times_5863113009.csv"
+]
 
-sent = preprocess(ex)
-print(sent)
+#Read the data file into an RDD (starting with one for now)
+datafile = sc.textFile('data/data/abc_news_86680728811.csv')
+print("Rows in the csv: ", datafile.count())
+
+#Split each line by , to extract field values¶
+values = datafile.map(lambda x: x.split(','))
+columns = values.take(1)
+
+#Find the named entities and split into an array
+def get_continuous_chunks(text):
+    chunked = ne_chunk(pos_tag(word_tokenize(text)))
+    continuous_chunk = []
+    current_chunk = []
+    for i in chunked:
+            if type(i) == Tree:
+                    current_chunk.append(" ".join([token for token, pos in i.leaves()]))
+            if current_chunk:
+                    named_entity = " ".join(current_chunk)
+                    if named_entity not in continuous_chunk:
+                            continuous_chunk.append(named_entity)
+                            current_chunk = []
+            else:
+                    continue
+    return continuous_chunk
+
+my_sent = "How's the weather in New York and Brooklyn"
+print(get_continuous_chunks(my_sent))
